a85ad44fb561db04fec7f366c451ee87371aefaa
JaeHeeAhn <jaehee.ahn@navercorp.com>
2011-03-11 02:07:28 +0000

    [XSSFILTERSUS-48] 테스트 리소스 추가
    git-svn-id: http://devcode.nhncorp.com/svn/xssfilter/trunk@43 6f729d1c-b579-0410-8f04-ce258326763b

---
+++ src/test/java/com/nhncorp/lucy/security/xss/listener/IMGListener.java
@@ -0,0 +1,27 @@
+package com.nhncorp.lucy.security.xss.listener;
+
+import com.nhncorp.lucy.security.xss.event.ElementListener;
+import com.nhncorp.lucy.security.xss.markup.Element;
+
+public class IMGListener implements ElementListener{
+
+	public void handleElement(Element e) {
+		
+		String id = e.getAttributeValue("id");
+		
+		String srcValue = "'http://local.cafe.naver.com/MoviePlayer.nhn?dir="+id+"?key=";
+		
+		e.removeAllAttributes();
+		
+		e.setName("iframe");
+		
+		e.putAttribute("frameborder", "'no'");
+		e.putAttribute("width", "342");
+		e.putAttribute("height", "296");
+		e.putAttribute("scrolling", "no");
+		e.putAttribute("name", "'mplayer'");
+		e.putAttribute("src", srcValue);
+		
+		e.setClose(true);
+	}
+}
