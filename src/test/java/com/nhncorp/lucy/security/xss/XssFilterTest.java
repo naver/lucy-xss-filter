f1bf186282ab6b9faebfe27e6a77df9befb2353f
nb10964 <nb10964@nhncorp.com>
2011-11-03 05:50:56 +0000

    [LUCYSUS-1589] [XSS Filter] 업무인수 및 시스템 파악(메뉴얼, 소스코드분석) add test code for study
    git-svn-id: http://devcode.nhncorp.com/svn/xssfilter/trunk@73 6f729d1c-b579-0410-8f04-ce258326763b

--- src/test/java/com/nhncorp/lucy/security/xss/XssFilterTest.java
+++ src/test/java/com/nhncorp/lucy/security/xss/XssFilterTest.java
@@ -1,7 +1,7 @@
 /*
- * @(#) XssFilterTest.java 2010. 8. 11 
+ * @(#) XssFilterTest.java 2010. 8. 11
  *
- * Copyright 2010 NHN Corp. All rights Reserved. 
+ * Copyright 2010 NHN Corp. All rights Reserved.
  * NHN PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
  */
 package com.nhncorp.lucy.security.xss;
@@ -13,9 +13,9 @@ import org.junit.Test;
 
 /**
  * {@link XssFilter} 기능 점검을 위한 테스트 코드.
- * 
+ *
  * 공격적인 코드와 완전하지 않은 HTML을 필터링 하는지와, 정상적인 HTML을 원형 그대로 보존하는지 검사한다.
- * 
+ *
  * @author Web Platform Development Team
  */
 public class XssFilterTest extends XssFilterTestCase {
@@ -47,7 +47,6 @@ public class XssFilterTest extends XssFilterTestCase {
 	// 시스템을 공격하는 코드를 필터링 하는지 검사한다.(필터링 전후가 틀려야 정상)
 	public void testCrackCodeFiltering() throws Exception {
 		XssFilter filter = XssFilter.getInstance("lucy-xss-mine.xml");
-		int i = 1;
 		for (String invalid : readString(INVALID_HTML_FILES)) {
 			String clean = filter.doFilter(invalid);
 			Assert.assertFalse("\n" + invalid + "\n" + clean, invalid.equals(clean));
@@ -73,6 +72,11 @@ public class XssFilterTest extends XssFilterTestCase {
 		String sameClean = sameFilter.doFilter(dirty);
 		String otherClean = otherFilter.doFilter(dirty);
 
+		System.out.println("dirty : " + dirty);
+		System.out.println("clean : " + clean);
+		System.out.println("sameClean : " + sameClean);
+		System.out.println("otherClean : " + otherClean);
+
 		Assert.assertFalse("\n" + dirty + "\n" + clean, dirty.equals(clean));
 		Assert.assertTrue("\n" + clean + "\n" + sameClean, clean.equals(sameClean));
 		Assert.assertFalse("\n" + clean + "\n" + otherClean, clean.equals(otherClean));
@@ -90,7 +94,7 @@ public class XssFilterTest extends XssFilterTestCase {
 	}
 
 	@Test
-	// White Url을 포함한 Embed 태그에 대한 보안 필터링 하는지 검사한다. 
+	// White Url을 포함한 Embed 태그에 대한 보안 필터링 하는지 검사한다.
 	public void testEmbedListenerWithWhiteUrl() throws Exception {
 		XssFilter filter = XssFilter.getInstance("lucy-xss3.xml");
 
@@ -120,91 +124,86 @@ public class XssFilterTest extends XssFilterTestCase {
 		Assert.assertNotNull(filter.doFilter("embeded", "param", "param"));
 	}
 
-	
-
 	@Test
 	//lucy-xss-superset.xml <notAllowedPattern><![CDATA[&[#\\%x]+[\da-fA-F][\da-fA-F]+]]></notAllowedPattern> 수정
 	//그 결과 COLOR 색상표(#16진수)는 필터링하지 않는다.
 	public void testSuperSetFix() {
 		XssFilter filter = XssFilter.getInstance("lucy-xss-superset.xml");
-		String clean ="<TABLE class=\"NHN_Layout_Main\" style=\"TABLE-LAYOUT: fixed\" cellSpacing=\"0\" cellPadding=\"0\" width=\"743\">" +
-				"</TABLE>" +
-				"<SPAN style=\"COLOR: #66cc99\"></SPAN>";
+		String clean = "<TABLE class=\"NHN_Layout_Main\" style=\"TABLE-LAYOUT: fixed\" cellSpacing=\"0\" cellPadding=\"0\" width=\"743\">" + "</TABLE>" + "<SPAN style=\"COLOR: #66cc99\"></SPAN>";
 		String filtered = filter.doFilter(clean);
 		Assert.assertEquals(clean, filtered);
 	}
-	
+
 	@Test
 	//EndTag가 없는 HTML이 입력으로 들어왔을 때 필터링한다. (WhiteList File의 Element 속성 EndTag 값이 true 인 경우)
 	public void testEndTagFilter() {
 		XssFilter filter = XssFilter.getInstance("lucy-xss-superset.xml");
-		String dirty = "<p><FONT style=\"FONT-SIZE: 9pt; FONT-FAMILY: 1144591_9\">"
-			+ "<FONT style=\"FONT-SIZE: 9pt; FONT-FAMILY: 1144591_9\">"
-			+ "<FONT style=\"FONT-SIZE: 10pt; FONT-FAMILY: 1144591_10\"> 에서 탑승하시오.</FONT></FONT></P>";
+		String dirty = "<p><FONT style=\"FONT-SIZE: 9pt; FONT-FAMILY: 1144591_9\">" + "<FONT style=\"FONT-SIZE: 9pt; FONT-FAMILY: 1144591_9\">" + "<FONT style=\"FONT-SIZE: 10pt; FONT-FAMILY: 1144591_10\"> 에서 탑승하시오.</FONT></FONT></P>";
 		String clean = filter.doFilter(dirty);
 		String unexpected = dirty;
+		System.out.println("dirty : " + dirty);
+		System.out.println("clean : " + clean);
 		Assert.assertNotSame(unexpected, clean);
-	}		
-		
+	}
+
 	@Test
 	//HTML5 적용된 브라우저에서 Base64 인코딩된 XSS 우회 공격을 필터링한다.
 	public void testBase64DecodingTest() {
-		
+
 		XssFilter filter = XssFilter.getInstance("xss.xml");
-		String dirty ="<embed src=\"data:text/html;base64,PHNjcmlwdD5hbGVydCgnZW1iZWRfc2NyaXB0X2FsZXJ0Jyk8L3NjcmlwdD4=\">";
-		String expected ="<!-- Not Allowed Attribute Filtered --><embed>";
+		String dirty = "<embed src=\"data:text/html;base64,PHNjcmlwdD5hbGVydCgnZW1iZWRfc2NyaXB0X2FsZXJ0Jyk8L3NjcmlwdD4=\">";
+		String expected = "<!-- Not Allowed Attribute Filtered --><embed>";
 		String clean = filter.doFilter(dirty);
 		Assert.assertEquals(expected, clean);
-		
-		String dirty2 ="<object data=\"data:text/html;base64,PHNjcmlwdD5hbGVydCgnb2JqZWN0X3NjcmlwdF9hbGVydCcpPC9zY3JpcHQ+\"></object>";
-		String expected2 ="<!-- Not Allowed Attribute Filtered --><object></object>";
+
+		String dirty2 = "<object data=\"data:text/html;base64,PHNjcmlwdD5hbGVydCgnb2JqZWN0X3NjcmlwdF9hbGVydCcpPC9zY3JpcHQ+\"></object>";
+		String expected2 = "<!-- Not Allowed Attribute Filtered --><object></object>";
 		String clean2 = filter.doFilter(dirty2);
 		Assert.assertEquals(expected2, clean2);
 	}
-	
+
 	@Test
 	// Element Class의 setName Method와 removeAllAttributes Method를 테스트한다.
 	// IMGLinstener에서 해당 메소드를 호출하여 IMG를 iframe으로 변경하고, IMG의 모든 속성을 제거한 후 원하는 속성으로 변경한다.
-	public void testRemoveAllAttributesTest(){
-		
+	public void testRemoveAllAttributesTest() {
+
 		XssFilter filter = XssFilter.getInstance("lucy-xss-cafe-child.xml");
 
 		String dirty = "<IMG id=mms://stream.media.naver.com/cafeucc2/2007/8/6/41/46b6e5b82fd46b6e5c23c8-danyecafe.wmv height=284 src=\"http://thumb.media.naver.com/cafeucc2/2007/8/6/41/46b6e5b82fd46b6e5c23c8-danyecafe_player.jpg\" width=342 movietype=\"1\">";
 		String expected = "<iframe frameborder='no' width=342 height=296 scrolling=no name='mplayer' src='http://local.cafe.naver.com/MoviePlayer.nhn?dir=mms://stream.media.naver.com/cafeucc2/2007/8/6/41/46b6e5b82fd46b6e5c23c8-danyecafe.wmv?key=></iframe>";
 		String clean = filter.doFilter(dirty);
 		Assert.assertTrue("\n" + dirty + "\n" + clean + "\n" + expected, expected.equals(clean));
-		
+
 		dirty = "<IMG></IMG>";
 		expected = "<iframe></iframe>";
 		String actual = filter.doFilter(dirty);
 		Assert.assertEquals(expected, actual);
-		
+
 	}
-	
+
 	@Test
-	public void testASCIICtrlChars(){
+	public void testASCIICtrlChars() {
 		// ASCIICtrl Chars : URL encoded %00 ~ %1F, %7F 이중 문제가 되는 것은 %00 뿐이다.
 		XssFilter filter = XssFilter.getInstance();
 
 		String dirty = URLDecoder.decode("%00");
 		String expected = "\0";
 		String clean = filter.doFilter(dirty);
-		
+
 		Assert.assertTrue(expected.equals(clean));
-		
-		String dirty2="aaa\0aaa";
+
+		String dirty2 = "aaa\0aaa";
 		String expected2 = "aaa\0aaa";
 		String clean2 = filter.doFilter(dirty2);
-		
+
 		Assert.assertTrue(expected2.equals(clean2));
-		
-		String dirty3="\0aaa\0\0aaa\0";
+
+		String dirty3 = "\0aaa\0\0aaa\0";
 		String expected3 = "\0aaa\0\0aaa\0";
 		String clean3 = filter.doFilter(dirty3);
-		
+
 		Assert.assertTrue(expected3.equals(clean3));
-	
-	
+
 	}
 
 	// startTag에서 공백 뒤에 오는 Close char '/' 를 attributeName으로 인식하는 오류 수정
@@ -212,49 +211,43 @@ public class XssFilterTest extends XssFilterTestCase {
 	// 공백 + /> 가 표준이므로 공백 없이 '/' char가 온 경우도 공백을 넣어서 리턴하도록 함.
 	// e.g. <br/> -> <br />
 	@Test
-	public void testXHTMLStandard () {
+	public void testXHTMLStandard() {
 		XssFilter filter = XssFilter.getInstance("lucy-xss-superset.xml");
-		
+
 		String dirty = "<br />";
 		String clean = filter.doFilter(dirty);
-		
+
 		Assert.assertTrue(dirty.equals(clean));
-		
+
 		String dirty2 = "<img src=\"aaaa\" />";
 		String clean2 = filter.doFilter(dirty2);
-		
+
 		Assert.assertTrue(dirty2.equals(clean2));
-		
+
 		String dirty3 = "<br/>";
 		String expected3 = "<br />";
 		String clean3 = filter.doFilter(dirty3);
 		Assert.assertTrue(expected3.equals(clean3));
-		
+
 		String dirty4 = "<img src=\"aaaa\"/>";
 		String expected4 = "<img src=\"aaaa\" />";
 		String clean4 = filter.doFilter(dirty4);
-		
+
 		Assert.assertTrue(expected4.equals(clean4));
-		
-		String dirty5 = "<p>"
-			+"<FONT style=\"FONT-SIZE: 9pt; FONT-FAMILY: 1144591_9\">"
-			+"<FONT style=\"FONT-SIZE: 10pt; FONT-FAMILY: 1144591_10\"> 에서 탑승하시오.</FONT>"
-			+"<br />"
-			+"</FONT>" 
-			+"</p>";
-		
+
+		String dirty5 = "<p>" + "<FONT style=\"FONT-SIZE: 9pt; FONT-FAMILY: 1144591_9\">" + "<FONT style=\"FONT-SIZE: 10pt; FONT-FAMILY: 1144591_10\"> 에서 탑승하시오.</FONT>" + "<br />" + "</FONT>" + "</p>";
+
 		String clean5 = filter.doFilter(dirty5);
 		Assert.assertTrue(clean5.equals(dirty5));
-		
+
 		String dirty6 = "<img src=\"aaaa\" >";
 		String expected6 = "<img src=\"aaaa\">";
 		String clean6 = filter.doFilter(dirty6);
-		
+
 		Assert.assertTrue(expected6.equals(clean6));
-		
+
 	}
-	
-	
+
 	@Test
 	//VM 옵션 -Xss 128k 에서 overflow 발생하는 사례 / -Xss 256k or Defalut(512k) 옵션에서는 정상 작동
 	public void testCafeHtmlFiltering() throws Exception {
@@ -264,78 +257,77 @@ public class XssFilterTest extends XssFilterTestCase {
 			//Assert.assertTrue("\n" + valid + "\n" + clean, valid.equals(clean));
 		}
 	}
-	
+
 	@Test
 	//필터링된 Tag와 Attribute 입력에 Comment를 삽입하는 것은 옵션으로한다.
 	//getInstance의 두번째 파라미터가 noComment를 설정할 수 있다.
 	public void testNoCommentXSSFilter() {
-		
+
 		XssFilter filter = XssFilter.getInstance("lucy-xss5.xml", true);
-		String dirty ="<embed src=\"data:text/html;base64,PHNjcmlwdD5hbGVydCgnZW1iZWRfc2NyaXB0X2FsZXJ0Jyk8L3NjcmlwdD4=\"></embed>";
-		String expected ="<embed></embed>";
+		String dirty = "<embed src=\"data:text/html;base64,PHNjcmlwdD5hbGVydCgnZW1iZWRfc2NyaXB0X2FsZXJ0Jyk8L3NjcmlwdD4=\"></embed>";
+		String expected = "<embed></embed>";
 		String clean = filter.doFilter(dirty);
 		Assert.assertEquals(expected, clean);
-		
-		String dirty2 ="<script></script>";
-		String expected2 ="&lt;script&gt;&lt;/script&gt;";
+
+		String dirty2 = "<script></script>";
+		String expected2 = "&lt;script&gt;&lt;/script&gt;";
 		String clean2 = filter.doFilter(dirty2);
 		Assert.assertEquals(expected2, clean2);
 	}
 
-	
 	@Test
 	public void testDetectedBase64EncodingAttect() {
 		XssFilter filter = XssFilter.getInstance("lucy-xss-nelo.xml");
-		
+
 		String dirty = "<EMBED SRC=\"data:image/svg+xml;base64,PHN2ZyB4bWxuczpzdmc9Imh0dH A6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcv MjAwMC9zdmciIHhtbG5zOnhsaW5rPSJodHRwOi8vd3d3LnczLm9yZy8xOTk5L3hs aW5rIiB2ZXJzaW9uPSIxLjAiIHg9IjAiIHk9IjAiIHdpZHRoPSIxOTQiIGhlaWdodD0iMjAw IiBpZD0ieHNzIj48c2NyaXB0IHR5cGU9InRleHQvZWNtYXNjcmlwdCI+YWxlcnQoIlh TUyIpOzwvc2NyaXB0Pjwvc3ZnPg==\" type=\"image/svg+xml\" AllowScriptAccess=\"always\"></EMBED>";
-		
+
 		String clean = filter.doFilter(dirty);
-		String expected ="<!-- Not Allowed Attribute Filtered --><EMBED type=\"image/svg+xml\"></EMBED>";
+		String expected = "<!-- Not Allowed Attribute Filtered --><EMBED type=\"image/svg+xml\"></EMBED>";
 		Assert.assertEquals(expected, clean);
 	}
-	
+
 	@Test
 	//DOCTYPE과 xml 태그를 허용하도록한다.
 	public void testDOCTYPEAndXMLELEMNT() {
-		
+
 		XssFilter filter = XssFilter.getInstance("lucy-xss-mail.xml");
-		
+
 		String doctype = "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">";
-		String clean = filter.doFilter(doctype);		
+		String clean = filter.doFilter(doctype);
 		Assert.assertEquals(doctype, clean);
-		
+
 		String xmltag = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>";
 		clean = filter.doFilter(xmltag);
 		Assert.assertEquals(xmltag, clean);
-		
+
 		String xssElement = "<script></script>";
 		String expected = "<blocking_script></blocking_script>";
 		clean = filter.doFilter(xssElement);
 		Assert.assertEquals(expected, clean);
-			
+
 	}
-	
+
 	@Test
 	public void testOverrideIssue() {
 		XssFilter filter = XssFilter.getInstance("lucy-xss-body-test.xml");
 		String testDirty = "<div><o:p><FONT face=\"맑은 고딕\">&nbsp;</FONT></o:p></div><span lang=EN-US>TEST</span>";
 		String clean = filter.doFilter(testDirty);
 		Assert.assertEquals(testDirty, clean);
-		
+
 		testDirty = "<p><newElement1></newElement1></p>";
 		clean = filter.doFilter(testDirty);
 		Assert.assertEquals(testDirty, clean);
-		
+
 		testDirty = "<body><embed></embed></body>";
 		clean = filter.doFilter(testDirty);
 		Assert.assertEquals(testDirty, clean);
-		
+
 		testDirty = "<a bordercolordark=b></a>";
 		clean = filter.doFilter(testDirty);
 		Assert.assertEquals(testDirty, clean);
-		
+
 	}
-	
+
 	@Test
 	//IEHack을 허용한다.
 	public void testIEHackTag() {
@@ -344,24 +336,38 @@ public class XssFilterTest extends XssFilterTestCase {
 		//String dirty = "<!--[if gte mso 9]><style>v\\:* {behavior:url(#default#VML);} o\\:* {behavior:url(#default#VML);} w\\:* {behavior:url(#default#VML);} .shape {behavior:url(#default#VML);} </style><![endif]-->";
 		String clean = filter.doFilter(dirty);
 		Assert.assertEquals(dirty, clean);
-		
+
 		dirty = "<!--[if !IE]> <-->";
 		clean = filter.doFilter(dirty);
 		String expected = "<!--[if !IE]> <!-- Not Allowed Tag Filtered -->&lt;--&gt;";
 		Assert.assertEquals(expected, clean);
-		
+
 		dirty = "<!--> <![endif]-->";
 		clean = filter.doFilter(dirty);
 		expected = "<!--&gt; &lt;![endif]-->";
 		Assert.assertEquals(expected, clean);
-		
+
 	}
-	
+
+	@Test
+	public void testIEHackTagOtherCase() {
+		XssFilter xssFilter = XssFilter.getInstance("lucy-xss-default.xml");
+		String dirty = "<!--[if !supportMisalignedColumns]--> <style> div { border:1px solid #f00; } </style> <!--[endif]-->";
+		String clean = xssFilter.doFilter(dirty);
+
+		Assert.assertEquals(dirty, clean);
+
+		dirty = " <!--[if !supportMisalignedColumns]> <style> div { border:1px solid #f00; } </style><![endif]-->";
+		clean = xssFilter.doFilter(dirty);
+
+		Assert.assertEquals(dirty, clean);
+	}
+
 	@Test
 	//Element Class의 remveAllContents Method를 테스트한다.
 	//StyleListener에서 해당 메소드를 호출하여 style 태그의 하위에 속하는 모든 child를 제거한다.
 	public void testRemoveAllContents() {
-		
+
 		XssFilter filter = XssFilter.getInstance("lucy-xss-mail.xml");
 		String dirty = "<!--[if !mso]><style>v\\:* {behavior:url(#default#VML);} o\\:* {behavior:url(#default#VML);} w\\:* {behavior:url(#default#VML);} .shape {behavior:url(#default#VML);} </style><![endif]-->";
 		String clean = filter.doFilter(dirty);
@@ -420,15 +426,15 @@ public class XssFilterTest extends XssFilterTestCase {
 	public void testOnMouseFilter() {
 
 		XssFilter filter = XssFilter.getInstance("lucy-xss-on.xml");
-		
-		String dirty1 =" onmouseover=prompt(954996)";
+
+		String dirty1 = " onmouseover=prompt(954996)";
 		String dirty2 = URLDecoder.decode("%22%20onmouseover%3dprompt%28954996%29%20bad%3d%22");
-		String dirty3 ="\" onmouseover=prompt(954996)\'aa";
-		
+		String dirty3 = "\" onmouseover=prompt(954996)\'aa";
+
 		String clean1 = filter.doFilter("paramT", "paramA", dirty1);
 		String clean2 = filter.doFilter("paramT", "paramA", dirty2);
 		String clean3 = filter.doFilter("paramT", "paramA", dirty3);
-		
+
 		Assert.assertEquals("", clean1);
 		Assert.assertEquals("", clean2);
 		Assert.assertEquals("\"", clean3);
@@ -436,13 +442,13 @@ public class XssFilterTest extends XssFilterTestCase {
 		String orgKeyword = dirty2;//dirty3; //keyword 파라미터에 사용자가 입력하는 경우를 예로 든다.
 
 		XssFilter filter1 = XssFilter.getInstance("lucy-xss-on.xml"); // 새로운 사용자 정의 whitelist file을 만든다 (아래 첨부 내용 참고)
-		//이 API 는 version 1.1.0부터 제공합니다. 그 이하 version에서는 기존 방법대로 사용하세요.  단, lucy-xss.xml에 Element와 Attribute정의를 추가해야합니다.              
+		//이 API 는 version 1.1.0부터 제공합니다. 그 이하 version에서는 기존 방법대로 사용하세요.  단, lucy-xss.xml에 Element와 Attribute정의를 추가해야합니다.
 
-		String[] attList = orgKeyword.split("[\"'`]"); 
+		String[] attList = orgKeyword.split("[\"'`]");
 
 		boolean resultflag = true;
 
-		for(String att : attList){
+		for (String att : attList) {
 
 			att = "\"" + att + "\"";
 
@@ -450,17 +456,17 @@ public class XssFilterTest extends XssFilterTestCase {
 
 			if (!att.equals(cleanAtt)) {
 				resultflag = false;
-			} 
+			}
 		}
 
 		String result = "";
 
-		if(resultflag){
+		if (resultflag) {
 			result = "Clean User!!";
-		}else{
+		} else {
 			result = "Dirty User !!";
 		}
-		
+
 		Assert.assertEquals("Dirty User !!", result);
 	}
 }
