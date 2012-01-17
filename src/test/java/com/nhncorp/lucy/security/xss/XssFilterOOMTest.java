d091c6ce36a31f6734cc624031d49110d40066fb
seongminwoo <seongmin.woo@navercorp.com>
2012-01-17 05:56:23 +0000

    [XSSFILTERSUS-77]  [메일웹개발팀] 메일 본문 html이 매우 큰 경우 lucy xss filter 적용시 OutOfMemoryError 가 발생에 대한 검토 테스트 코드 커밋
    git-svn-id: http://devcode.nhncorp.com/svn/xssfilter/trunk@80 6f729d1c-b579-0410-8f04-ce258326763b

---
+++ src/test/java/com/nhncorp/lucy/security/xss/XssFilterOOMTest.java
@@ -0,0 +1,59 @@
+/*
+ * @(#) XssFilterTest.java 2010. 8. 11
+ *
+ * Copyright 2010 NHN Corp. All rights Reserved.
+ * NHN PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+package com.nhncorp.lucy.security.xss;
+
+import java.io.StringWriter;
+import java.util.LinkedList;
+
+import org.htmlcleaner.CleanerProperties;
+import org.htmlcleaner.HtmlCleaner;
+import org.htmlcleaner.PrettyHtmlSerializer;
+import org.htmlcleaner.TagNode;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * 대용량 파일 처리 시 {@link XssFilter} OOM 분석을 위한 테스트 코드.
+ * 메일웹 개발팀 문의 사항.
+ * @author Web Platform Development Team
+ */
+public class XssFilterOOMTest extends XssFilterTestCase {
+	private static final String BIG_HTML_FILES = "본문이_큰_html_메일.eml";
+
+	@Test
+	public void testHtmlFiltering() throws Exception {
+		XssFilter filter = XssFilter.getInstance();
+		String target = readString(BIG_HTML_FILES);
+		String clean = filter.doFilter(target);
+		clean = filter.doFilter(target);
+		clean = filter.doFilter(target);
+		clean = filter.doFilter(target);
+		clean = filter.doFilter(target);
+		clean = filter.doFilter(target);
+		clean = filter.doFilter(target);
+		clean = filter.doFilter(target);
+		clean = filter.doFilter(target);
+		clean = filter.doFilter(target);
+		clean = filter.doFilter(target);
+		clean = filter.doFilter(target);
+		clean = filter.doFilter(target);
+		clean = filter.doFilter(target);
+		clean = filter.doFilter(target);
+		clean = filter.doFilter(target);
+		//Assert.assertTrue("\n" + target + "\n" + clean, target.equals(clean));
+	}
+	
+	@Test
+	public void testHtmlFilteringOtherFiles() throws Exception {
+		XssFilter filter = XssFilter.getInstance();
+		String target = readString(BIG_HTML_FILES);
+		String clean = filter.doFilter(target);
+		target = readString("html_본문이지만_태그가_없는_경우.eml");
+		clean = filter.doFilter(target);
+		//Assert.assertTrue("\n" + target + "\n" + clean, target.equals(clean));
+	}
+}
