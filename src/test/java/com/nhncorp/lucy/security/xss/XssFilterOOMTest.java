8b0b681191db639cae813520424cf0630d9ddf7b
nb10964 <nb10964@nhncorp.com>
2012-02-22 06:21:24 +0000

    [JAMIESUS-152] Jamie Monitoring OOM 테스트 코드
    git-svn-id: http://devcode.nhncorp.com/svn/xssfilter/trunk@84 6f729d1c-b579-0410-8f04-ce258326763b

--- src/test/java/com/nhncorp/lucy/security/xss/XssFilterOOMTest.java
+++ src/test/java/com/nhncorp/lucy/security/xss/XssFilterOOMTest.java
@@ -6,6 +6,13 @@
  */
 package com.nhncorp.lucy.security.xss;
 
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.Writer;
+
+import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
 
 /**
@@ -14,38 +21,79 @@ import org.junit.Test;
  * @author Web Platform Development Team
  */
 public class XssFilterOOMTest extends XssFilterTestCase {
-	private static final String BIG_HTML_FILES = "본문이_큰_html_메일.eml";
+	private static final String BIG_HTML_FILES_6M = "본문이 큰 html 메일_6M.html";
+	private static final String BIG_HTML_FILES_62M = "본문이 큰 html 메일_62M.html";
+	private static final String BIG_EML_FILES_8M = "본문이_큰_html_메일.eml";
+	private static final String BIG_HTML_FILES_6M_REMOVE_IEHACK = "본문이 큰 html 메일_IEHack제거_6M.html";
+	private static final String BIG_HTML_FILES_6M_REMOVE_HTML_BODY_TAG = "본문이 큰 html 메일_6M_remove_html_body_tag.html";
+	private static final String BIG_HTML_FILES_3M = "본문이 큰 html 메일_용량줄임.html";
 
+	@Ignore
 	@Test
 	public void testHtmlFiltering() throws Exception {
 		XssFilter filter = XssFilter.getInstance();
-		String target = readString(BIG_HTML_FILES);
+		String target = readString(BIG_EML_FILES_8M);
 		String clean = filter.doFilter(target);
-		clean = filter.doFilter(target);
-		clean = filter.doFilter(target);
-		clean = filter.doFilter(target);
-		clean = filter.doFilter(target);
-		clean = filter.doFilter(target);
-		clean = filter.doFilter(target);
-		clean = filter.doFilter(target);
-		clean = filter.doFilter(target);
-		clean = filter.doFilter(target);
-		clean = filter.doFilter(target);
-		clean = filter.doFilter(target);
-		clean = filter.doFilter(target);
-		clean = filter.doFilter(target);
-		clean = filter.doFilter(target);
-		clean = filter.doFilter(target);
-		//Assert.assertTrue("\n" + target + "\n" + clean, target.equals(clean));
+		
 	}
 	
 	@Test
-	public void testHtmlFilteringOtherFiles() throws Exception {
+	public void testOnlyOneElementCase() throws Exception {
 		XssFilter filter = XssFilter.getInstance();
-		String target = readString(BIG_HTML_FILES);
+		String target = "<h1>Hello</h1>";
 		String clean = filter.doFilter(target);
-		target = readString("html_본문이지만_태그가_없는_경우.eml");
-		clean = filter.doFilter(target);
-		//Assert.assertTrue("\n" + target + "\n" + clean, target.equals(clean));
+		long total = Runtime.getRuntime().totalMemory();
+		long free = Runtime.getRuntime().freeMemory();
+		long used = total - free;
+		System.out.println("Used memory: " + used);
+		System.out.println("clean : " + clean);
+	}
+	
+	@Ignore
+	@Test
+	public void test6MFileSizeCase() throws Exception {
+		XssFilter filter = XssFilter.getInstance();
+		String target = readString(BIG_HTML_FILES_6M);
+		String result = filter.doFilter(target);
+		Assert.assertTrue(target.equals(result));
+	}
+	
+	@Ignore
+	@Test
+	public void test3MFileSize() throws Exception {
+		XssFilter filter = XssFilter.getInstance();
+		String target = readString(BIG_HTML_FILES_3M);
+		//String result = filter.doFilter(target);
+		//Assert.assertTrue(target.equals(result));
+		Writer writer;
+		writer = new BufferedWriter(new FileWriter("d:/test3MFileSizeForEachTokenWayOldRule.html"));
+		//writer = new BufferedWriter(new FileWriter("d:/test3MFileSizeForAllTokenWay2.html"));
+		filter.doFilter(target, writer);
+	}
+	
+	@Ignore
+	@Test
+	public void test6MFileSizeRemoveIEHackCase() throws Exception {
+		XssFilter filter = XssFilter.getInstance();
+		String target = readString(BIG_HTML_FILES_6M_REMOVE_IEHACK);
+		String result = filter.doFilter(target);
+		Assert.assertTrue(target.equals(result));
+	}
+	
+	@Ignore
+	@Test
+	public void test6MFileSizeRemoveHtmlBodyTagCase() throws Exception {
+		XssFilter filter = XssFilter.getInstance();
+		String target = readString(BIG_HTML_FILES_6M_REMOVE_HTML_BODY_TAG);
+		String result = filter.doFilter(target);
+		Assert.assertTrue(target.equals(result));
+	}
+	
+	@Ignore
+	@Test
+	public void test62MFileSizeCase() throws Exception {
+		XssFilter filter = XssFilter.getInstance();
+		String target = readString(BIG_HTML_FILES_62M);
+		filter.doFilter(target);
 	}
 }
