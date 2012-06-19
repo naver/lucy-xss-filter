d7a59a70f41737c0c2751f26f97cfef1d59844c8
seongminwoo <seongmin.woo@navercorp.com>
2012-06-19 02:56:05 +0000

    [XSSFILTERSUS-117] [릴리즈] 1.3.0 릴리즈 작업 commons-io 의존성 제거를 위해 사용되는 메소드 getExtension 소스코드를 복사하고 의존성은 제거.
    git-svn-id: http://devcode.nhncorp.com/svn/xssfilter/trunk@153 6f729d1c-b579-0410-8f04-ce258326763b

--- src/main/java/com/nhncorp/lucy/security/xss/listener/SecurityUtils.java
+++ src/main/java/com/nhncorp/lucy/security/xss/listener/SecurityUtils.java
@@ -11,7 +11,6 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Date;
 
-import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.lang.StringUtils;
 
 import com.nhncorp.lucy.security.xss.Constants;
@@ -22,6 +21,22 @@ import com.nhncorp.lucy.security.xss.markup.Element;
  */
 public class SecurityUtils {
 	private final static char[] specialCharArray = "?&=".toCharArray();
+	 /**
+     * The extension separator character.
+     * @since Commons IO 1.4
+     */
+    public static final char EXTENSION_SEPARATOR = '.';
+
+    /**
+     * The Unix separator character.
+     */
+    private static final char UNIX_SEPARATOR = '/';
+
+    /**
+     * The Windows separator character.
+     */
+    private static final char WINDOWS_SEPARATOR = '\\';
+
 	
 	/**
 	 * @param element
@@ -48,7 +63,7 @@ public class SecurityUtils {
 			} else {
 				//확장자 체크
 				String url = StringUtils.strip(srcUrl, "'\"");
-				String extension = FilenameUtils.getExtension(url);
+				String extension = getExtension(url);
 				
 				if (StringUtils.containsAny(extension, specialCharArray)) {
 					int pos = StringUtils.indexOfAny(extension, specialCharArray);
@@ -100,7 +115,7 @@ public class SecurityUtils {
 			} else {
 				//확장자 체크
 				String url = StringUtils.strip(srcUrl, "'\"");
-				String extension = FilenameUtils.getExtension(url);
+				String extension = getExtension(url);
 				
 				if (StringUtils.containsAny(extension, specialCharArray)) {
 					int pos = StringUtils.indexOfAny(extension, specialCharArray);
@@ -202,4 +217,77 @@ public class SecurityUtils {
 			return true;
 		}
 	}
+	
+	/**
+     * Gets the extension of a filename.
+     * <p>
+     * This method returns the textual part of the filename after the last dot.
+     * There must be no directory separator after the dot.
+     * <pre>
+     * foo.txt      --> "txt"
+     * a/b/c.jpg    --> "jpg"
+     * a/b.txt/c    --> ""
+     * a/b/c        --> ""
+     * </pre>
+     * <p>
+     * The output will be the same irrespective of the machine that the code is running on.
+     *
+     * @param filename the filename to retrieve the extension of.
+     * @return the extension of the file or an empty string if none exists or <code>null</code>
+     * if the filename is <code>null</code>.
+     */
+    public static String getExtension(String filename) {
+        if (filename == null) {
+            return null;
+        }
+        int index = indexOfExtension(filename);
+        if (index == -1) {
+            return "";
+        } else {
+            return filename.substring(index + 1);
+        }
+    }
+	
+	/**
+     * Returns the index of the last extension separator character, which is a dot.
+     * <p>
+     * This method also checks that there is no directory separator after the last dot.
+     * To do this it uses {@link #indexOfLastSeparator(String)} which will
+     * handle a file in either Unix or Windows format.
+     * <p>
+     * The output will be the same irrespective of the machine that the code is running on.
+     * 
+     * @param filename  the filename to find the last path separator in, null returns -1
+     * @return the index of the last separator character, or -1 if there
+     * is no such character
+     */
+    public static int indexOfExtension(String filename) {
+        if (filename == null) {
+            return -1;
+        }
+        int extensionPos = filename.lastIndexOf(EXTENSION_SEPARATOR);
+        int lastSeparator = indexOfLastSeparator(filename);
+        return (lastSeparator > extensionPos ? -1 : extensionPos);
+    }
+    
+    /**
+     * Returns the index of the last directory separator character.
+     * <p>
+     * This method will handle a file in either Unix or Windows format.
+     * The position of the last forward or backslash is returned.
+     * <p>
+     * The output will be the same irrespective of the machine that the code is running on.
+     * 
+     * @param filename  the filename to find the last path separator in, null returns -1
+     * @return the index of the last separator character, or -1 if there
+     * is no such character
+     */
+    public static int indexOfLastSeparator(String filename) {
+        if (filename == null) {
+            return -1;
+        }
+        int lastUnixPos = filename.lastIndexOf(UNIX_SEPARATOR);
+        int lastWindowsPos = filename.lastIndexOf(WINDOWS_SEPARATOR);
+        return Math.max(lastUnixPos, lastWindowsPos);
+    }
 }
