04dd9ac5338987dade16bf41535ca3f085645d1d
nb10964 <nb10964@nhncorp.com>
2012-08-06 01:44:45 +0000

    [XSSFILTERSUS-124] [EC서비스개발랩] ObjectListener, EmbedListener 에서 허용할 확장자를 추가할 수 있는 확장포인트(프로퍼티파일) 제공 테스트 코드 및 기능 추가
    git-svn-id: http://devcode.nhncorp.com/svn/xssfilter/trunk@159 6f729d1c-b579-0410-8f04-ce258326763b

---
+++ src/test/java/com/nhncorp/lucy/security/xss/listener/EmbedListenerOld.java
@@ -0,0 +1,45 @@
+/*
+ * @(#) EmbedListener.java 2010. 8. 11 
+ *
+ * Copyright 2010 NHN Corp. All rights Reserved. 
+ * NHN PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+package com.nhncorp.lucy.security.xss.listener;
+
+import com.nhncorp.lucy.security.xss.event.ElementListener;
+import com.nhncorp.lucy.security.xss.markup.Element;
+
+/**
+ * 이 클래스는 Embed 태그에 대한 보안 필터링을 수행한다.
+ *
+ * @author Web Platform Development Team
+ * 
+ */
+public class EmbedListenerOld implements ElementListener {
+
+	public void handleElement(Element e) {
+		e.putAttribute("invokeURLs", "\"false\"");
+		e.putAttribute("autostart", "\"false\"");
+		e.putAttribute("allowScriptAccess", "\"never\"");
+		
+		if (this.isWhiteUrl(e.getAttributeValue("src"))) {
+			e.putAttribute("allowNetworking", "\"all\"");
+		} else {
+			e.putAttribute("allowNetworking", "\"internal\"");
+		}
+		
+	}
+	
+	private boolean isWhiteUrl(String url) {
+		try {
+			WhiteUrlList list = WhiteUrlList.getInstance();
+			if (list.contains(url)) {
+				return true;
+			}
+		} catch (Exception e) {
+			// ignore
+		}
+		return false;
+	}
+
+}
