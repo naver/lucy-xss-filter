3e3b506e8e0ef73c5a89da053bd3757c06ffabab
JaeHeeAhn <jaehee.ahn@navercorp.com>
2013-01-22 10:21:11 +0000

    [XSSFILTERSUS-137] 주석내 태그 처리를 설정에 따라 적용하도록 기능 추가 리스너 설정 확인을 위한 테스트 코드 추가
    git-svn-id: http://devcode.nhncorp.com/svn/xssfilter/trunk@172 6f729d1c-b579-0410-8f04-ce258326763b

--- src/test/java/com/nhncorp/lucy/security/xss/FilteringTagInCommentTest.java
+++ src/test/java/com/nhncorp/lucy/security/xss/FilteringTagInCommentTest.java
@@ -41,6 +41,16 @@ public class FilteringTagInCommentTest {
 	}
 	
 	@Test
+	public void testConfigFilteringWithListener() throws IOException {
+
+		XssFilter filter = XssFilter.getInstance("lucy-xss-filteringtagincomment-listener.xml");
+		String dirty = "<!--주석안에 태그가 있는 경우에요. <h1>제목</h1> <img src='http://www.naver.com/test.img'>Listerner Test</img> <SCRIPT SRC=http://xxx/xss.js></SCRIPT> 필터링이 필요한가요? 원본을 유지하는게 좋은가요?-->";
+		String clean = filter.doFilter(dirty);
+		String expected = "<!--주석안에 태그가 있는 경우에요. <h1>제목</h1> <iframe frameborder='no' width=342 height=296 scrolling=no name='mplayer' src='http://local.cafe.naver.com/MoviePlayer.nhn?dir=?key=>Listerner Test</iframe> &lt;SCRIPT SRC=http://xxx/xss.js&gt;&lt;/SCRIPT&gt; 필터링이 필요한가요? 원본을 유지하는게 좋은가요?-->";
+		Assert.assertEquals(expected, clean);
+	}
+	
+	@Test
 	public void testConfigSax() throws IOException {
 
 		XssSaxFilter filter = XssSaxFilter.getInstance("lucy-xss-filteringtagincomment-sax.xml");
@@ -77,6 +87,19 @@ public class FilteringTagInCommentTest {
 		String dirty = "<!--주석안에 태그가 있는 경우에요. <h1>제목</h1> <SCRIPT SRC=http://xxx/xss.js></SCRIPT> 필터링이 필요한가요? 원본을 유지하는게 좋은가요?-->";
 		String clean = filter.doFilter(dirty);
 		String expected = "<!--주석안에 태그가 있는 경우에요. <h1>제목</h1> <SCRIPT SRC=http://xxx/xss.js></SCRIPT> 필터링이 필요한가요? 원본을 유지하는게 좋은가요?-->";
+
+		Assert.assertEquals(expected, clean);
+	}
+	
+	@Test
+	public void testConfigFilteringWithListenerSax() throws IOException {
+
+		XssSaxFilter filter = XssSaxFilter.getInstance("lucy-xss-filteringtagincomment-listener-sax.xml");
+		String dirty = "<!--주석안에 태그가 있는 경우에요. <h1>제목</h1> <img src='http://www.naver.com/test.img'>Listerner Test</img> <SCRIPT SRC=http://xxx/xss.js></SCRIPT> 필터링이 필요한가요? 원본을 유지하는게 좋은가요?-->";
+		String clean = filter.doFilter(dirty);
+		System.out.println(dirty);
+		System.out.println(clean);
+		String expected = "<!--주석안에 태그가 있는 경우에요. <h1>제목</h1> <iframe frameborder='no' width=342 height=296 scrolling=no name='mplayer' src='http://local.cafe.naver.com/MoviePlayer.nhn?dir=?key=>Listerner Test</iframe> &lt;SCRIPT SRC=http://xxx/xss.js&gt;&lt;/SCRIPT&gt; 필터링이 필요한가요? 원본을 유지하는게 좋은가요?-->";
 
 		Assert.assertEquals(expected, clean);
 	}
