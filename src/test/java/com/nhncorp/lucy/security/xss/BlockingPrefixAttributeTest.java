d787d79df6e8d4b7451146948c1e57ee410f0ba2
nb10980 <nb10980@nhncorp.com>
2013-03-06 09:07:48 +0000

    [XSSFILTERSUS-138] blockingPrefix 설정 사용 시 발생하는 XSS 보안 취약점 차단
    git-svn-id: http://devcode.nhncorp.com/svn/xssfilter/trunk@173 6f729d1c-b579-0410-8f04-ce258326763b

---
+++ src/test/java/com/nhncorp/lucy/security/xss/BlockingPrefixAttributeTest.java
@@ -0,0 +1,35 @@
+/*
+ * @(#)MailBlockingPrefixTest.java $version Mar 6, 2013
+ *
+ * Copyright 2007 NHN Corp. All rights Reserved. 
+ * NHN PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+
+package com.nhncorp.lucy.security.xss;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+/**
+ * @author nbp
+ */
+public class BlockingPrefixAttributeTest {
+
+	@Test
+	public void testBlockingPrefix() {
+		XssFilter filter = XssFilter.getInstance("lucy-xss-mail.xml");
+		String clean = filter.doFilter("<iframe onmouseover=\"alert('kkk');\"></iframe>");
+		String expected = "<!-- Not Allowed Attribute Filtered ( onmouseover=\"alert('kkk');\") --><blocking_iframe></blocking_iframe>";
+		Assert.assertEquals(expected, clean);
+	}
+	 
+	
+	@Test
+	public void testSAXBlockingPrefix() {
+		XssSaxFilter filter = XssSaxFilter.getInstance("lucy-xss-sax-body-mail.xml");
+		String clean = filter.doFilter("<iframe onmouseover=\"alert('kkk');\"></iframe>");
+		String expected = "<!-- Not Allowed Attribute Filtered ( onmouseover=\"alert('kkk');\") --><xiframe></xiframe>";
+		Assert.assertEquals(expected, clean);
+	}
+}
