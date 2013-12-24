da332ad60d9a878811858e043ddeb479f491a0a4
JaeHeeAhn <jaehee.ahn@navercorp.com>
2013-12-24 10:13:20 +0000

    [XSSFILTERSUS-141] default 화이트리스트 설정파일에서 A태그에 속성 style을 필터링하는 처리 추가
    git-svn-id: http://devcode.nhncorp.com/svn/xssfilter/trunk@193 6f729d1c-b579-0410-8f04-ce258326763b

--- src/test/java/com/nhncorp/lucy/security/xss/BlockingPrefixAttributeTest.java
+++ src/test/java/com/nhncorp/lucy/security/xss/BlockingPrefixAttributeTest.java
@@ -27,6 +27,29 @@ public class BlockingPrefixAttributeTest {
 		String expected2 = "<!-- Not Allowed Attribute Filtered ( onmouseover=\"alert('kkk');\") --><blocking_iframe></blocking_iframe>";
 		Assert.assertEquals(expected2, clean2);
 	}
+	
+	@Test
+	public void testATagStyleAttrExclusion() {
+		XssFilter filter = XssFilter.getInstance("lucy-xss-mail.xml");
+		String clean = filter.doFilter("<a href=\"악성페이지URL\" style=\"display: block; z-index: 100000; opacity: 0.5;position: fixed; top: 0px; left: 0; width: 1000000px; height: 100000px;background-color: red;\"> </a>");
+		String expected = "<a href=http style=color> </a>";
+		Assert.assertEquals(expected, clean);
+		
+		XssSaxFilter saxfilter = XssSaxFilter.getInstance("lucy-xss-sax-cafe-child.xml");
+		String saxclean = saxfilter.doFilter("<a href=\"악성페이지URL\" style=\"display: block; z-index: 100000; opacity: 0.5;position: fixed; top: 0px; left: 0; width: 1000000px; height: 100000px;background-color: red;\"> </a>");
+		String saxexpected = "<a href=http style=color> </a>";
+		Assert.assertEquals(saxexpected, saxclean);
+		
+		XssFilter filter2 = XssFilter.getInstance();
+		String clean2 = filter2.doFilter("<a href=\"악성페이지URL\" style=\"display: block;\"><span style=\"display: block;\"></span></a>");
+		String expected2= "<!-- Not Allowed Attribute Filtered ( style=\"display: block;\") --><a href=\"악성페이지URL\"><span style=\"display: block;\"></span></a>";
+		Assert.assertEquals(expected2, clean2);
+		
+		XssSaxFilter saxfilter2 = XssSaxFilter.getInstance();
+		String saxclean2 = saxfilter2.doFilter("<a href=\"악성페이지URL\" style=\"display: block;\"><span style=\"display: block;\"></span></a>");
+		String saxexpected2 = "<!-- Not Allowed Attribute Filtered ( style=\"display: block;\") --><a href=\"악성페이지URL\"><span style=\"display: block;\"></span></a>";
+		Assert.assertEquals(saxexpected2, saxclean2);
+	}
 	 
 	
 	@Test
