2c2afad2a19b8ee6143642a1b10f8226fadf2755
leeplay <hyeongkyu.lee@navercorp.com>
2014-09-19 20:55:01 +0900

    [XSSFILTERSUS-154] apache common lang3 적용

--- src/main/java/com/nhncorp/lucy/security/xss/XssSaxFilter.java
+++ src/main/java/com/nhncorp/lucy/security/xss/XssSaxFilter.java
@@ -17,7 +17,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.regex.Pattern;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
