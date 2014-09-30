43315a5128fcd90c2dadb9ccd4de3cd8d89f28c1
leeplay <hyeongkyu.lee@navercorp.com>
2014-09-30 16:42:42 +0900

    [XSSFILTER-159] Add license notice

--- src/test/java/com/nhncorp/lucy/security/xss/BlockingPrefixAttributeTest.java
+++ src/test/java/com/nhncorp/lucy/security/xss/BlockingPrefixAttributeTest.java
@@ -1,10 +1,18 @@
 /*
- * @(#)MailBlockingPrefixTest.java $version Mar 6, 2013
- *
- * Copyright 2007 NHN Corp. All rights Reserved. 
- * NHN PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
- */
-
+ *	Copyright 2014 Naver Corp.
+ *	
+ *	Licensed under the Apache License, Version 2.0 (the "License");
+ *	you may not use this file except in compliance with the License.
+ *	You may obtain a copy of the License at
+ *	
+ *		http://www.apache.org/licenses/LICENSE-2.0
+ *	
+ *	Unless required by applicable law or agreed to in writing, software
+ *	distributed under the License is distributed on an "AS IS" BASIS,
+ *	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *	See the License for the specific language governing permissions and
+ *	limitations under the License.
+ */	
 package com.nhncorp.lucy.security.xss;
 
 import junit.framework.Assert;
